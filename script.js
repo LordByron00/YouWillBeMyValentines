70489d71d28ef7408ffc78cd0247e18ba97da117
Justine Bayron <Jbayron76@gmail.com>
2024-02-04 16:08:11 +0800

    Lelouch version

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ const buttonsContainer = document.querySelector(".buttons");
 const yesButton = document.querySelector(".btn--yes");
 const noButton = document.querySelector(".btn--no");
 const catImg = document.querySelector(".cat-img");
+const messages = [
+  "No",
+  "Are you sure?",
+  "Pookie please",
+  "Don't do this to me :(",
+  "You're breaking my heart",
+  "I'm gonna cry...",
+  "Puppy eyes activated, how about a yes?",
+  "Nuh-uh",
+  "Oh, fluffy muffins, say yes?",
+  "Don't be a heartbreaker :(",
+  "Please don't be a dream crusher",
+  "Tiny teardrops forming...",
+  "Really, really? :("
+];
 
 const MAX_IMAGES = 5;
 
@@ -14,16 +29,16 @@ let noCount = 0;
 yesButton.addEventListener("click", handleYesClick);
 
 noButton.addEventListener("click", function () {
-  if (play) {
+  if (noCount < MAX_IMAGES) {
     noCount++;
     const imageIndex = Math.min(noCount, MAX_IMAGES);
     changeImage(imageIndex);
-    resizeYesButton();
-    updateNoButtonText();
-    if (noCount === MAX_IMAGES) {
-      play = false;
+    // if (noCount === MAX_IMAGES) {
+      //   play = false;
+      // }
     }
-  }
+    updateNoButtonText();
+    resizeYesButton();
 });
 
 function handleYesClick() {
@@ -35,23 +50,17 @@ function handleYesClick() {
 function resizeYesButton() {
   const computedStyle = window.getComputedStyle(yesButton);
   const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
-  const newFontSize = fontSize * 1.6;
-
+  const newFontSize = fontSize + 60;
   yesButton.style.fontSize = `${newFontSize}px`;
 }
 
-function generateMessage(noCount) {
-  const messages = [
-    "No",
-    "Are you sure?",
-    "Pookie please",
-    "Don't do this to me :(",
-    "You're breaking my heart",
-    "I'm gonna cry...",
-  ];
-
-  const messageIndex = Math.min(noCount, messages.length - 1);
-  return messages[messageIndex];
+function generateMessage() {
+  // const messageIndex = Math.min(noCount, messages.length - 1);
+  const index = Math.floor(Math.random() * messages.length);
+  console.log(index);
+  const message = messages[index];
+  console.log(message);
+  return message;
 }
 
 function changeImage(image) {
@@ -59,5 +68,5 @@ function changeImage(image) {
 }
 
 function updateNoButtonText() {
-  noButton.innerHTML = generateMessage(noCount);
+  noButton.innerHTML = generateMessage();
 }
